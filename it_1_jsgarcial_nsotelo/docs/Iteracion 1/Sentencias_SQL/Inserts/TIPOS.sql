6c3e01e69c604e6b477591ff43b49b45d0598788
sebastian <sebastian@Sebastians-MacBook-Pro.local>
2018-04-05 18:44:30 -0500

    archivos insert

---
+++ it_1_jsgarcial_nsotelo/docs/Iteracion 1/Sentencias_SQL/Inserts/TIPOS.sql
@@ -0,0 +1,17 @@
+
+-- 
+-- INSERT DATA RELACION TIPOS
+-- 
+-- En este archivo se pueblan todas las tablas (RF 1, 2 y 3) para poder realizar pruebas
+-- 
+
+(luz|TV|agua|internet|comida|cocina|baño|apoyosocial|apoyoacademico)
+
+INSERT INTO TIPOS (ID, NOMBRE, COSTO) VALUES (1, 'luz', 15000.00)
+INSERT INTO TIPOS (ID, NOMBRE, COSTO) VALUES (1, 'TV', 15000.00)
+INSERT INTO TIPOS (ID, NOMBRE, COSTO) VALUES (1, 'agua', 15000.00)
+INSERT INTO TIPOS (ID, NOMBRE, COSTO) VALUES (1, 'internet', 15000.00)
+INSERT INTO TIPOS (ID, NOMBRE, COSTO) VALUES (1, 'comida', 15000.00)
+INSERT INTO TIPOS (ID, NOMBRE, COSTO) VALUES (1, 'baño', 15000.00)
+INSERT INTO TIPOS (ID, NOMBRE, COSTO) VALUES (1, 'apoyosocial', 15000.00)
+INSERT INTO TIPOS (ID, NOMBRE, COSTO) VALUES (1, 'apoyoacademico', 15000.00)
