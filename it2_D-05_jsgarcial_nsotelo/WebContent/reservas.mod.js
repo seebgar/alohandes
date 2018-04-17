bfd2107fc54b2e867495e19c14bfb70f6e2d6715
JUAN SEBASTIAN GARCIA LOPEZ <js.garcial1@sis.virtual.uniandes.edu.co>
2018-04-17 11:32:09 -0500

    front x8

--- it2_D-05_jsgarcial_nsotelo/WebContent/reservas.mod.js
+++ it2_D-05_jsgarcial_nsotelo/WebContent/reservas.mod.js
@@ -1,62 +1,55 @@
-
-
-
 (function (ng) {
 
     var mod = ng.module("reservaModule", ['ui.router']);
-    
+
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-        
+
         $urlRouterProvider.otherwise("/x");
-        
+
         $stateProvider
-                .state('reservas', {
-							url: "/reservas",
-							templateUrl: "panel_reservas.html",
-                            controller: 'reservasController'
-						}) 
-        
-                .state('perfil_cliente', {
-							url: "/perfil_cliente",
-							templateUrl: "data/perfil_cliente.html",
-                            controller: 'reservasController'
-						}) 
-        
-    
-    
-    
+            .state('reservas', {
+                url: "/reservas",
+                templateUrl: "panel_reservas.html",
+                controller: 'reservasController'
+            })
+
+            .state('perfil_cliente', {
+                url: "/perfil_cliente",
+                templateUrl: "data/perfil_cliente.html",
+                controller: 'reservasController'
+            })
+
+
+
+
     }]);
-    
+
 })(window.angular);
 
 
 
 (function (ng) {
-   
+
     var mod = ng.module("reservaModule");
     mod.constant("reservasContext", "api/reservas");
-    
+
     mod.controller('reservasController', ['$scope', '$http', 'reservasContext',
-        
+
         function ($scope, $http, reservasContext) {
             // http://localhost:8080/Alohandes_IT1/rest/personas/clientes/$scope.id_cliente/reservas
-            $scope.get = function() {
-                console.log($scope.id_cliente);
-                    $http.get('http://localhost:8080/Alohandes_IT1/rest/personas/clientes/' + $scope.id_cliente + '/reservas').then(function (response) {
-                    $scope.reservas = response.data;
-                });
-            }
-            
+            console.log($scope.id_cliente);
+            $http.get('http://localhost:8080/Alohandes_IT1/rest/personas/clientes/40/reservas').then(function (response) {
+                $scope.reservas = response.data;
+            });
+
             // http://localhost:8080/Alohandes_IT1/rest/personas/ 
-            $scope.get_2 = function() {
-                $http.get('http://localhost:8080/Alohandes_IT1/rest/personas/' + $scope.id_cliente).then(function (response) {
+            $http.get('http://localhost:8080/Alohandes_IT1/rest/personas/' + 40).then(function (response) {
                 $scope.cliente = response.data;
             });
-            }
-            
-            
+
+
+
         }
-                                          
+
     ]);
-}
-)(window.angular);
+})(window.angular);
