5820caef3942d82aa459c584d6156db743cb958b
JUAN SEBASTIAN GARCIA LOPEZ <js.garcial1@sis.virtual.uniandes.edu.co>
2018-04-23 19:03:33 -0500

    detalles postman

--- it2_D-05_jsgarcial_nsotelo/WebContent/modules/reservas/post_colectivas.mod.js
+++ it2_D-05_jsgarcial_nsotelo/WebContent/modules/reservas/post_colectivas.mod.js
@@ -1,71 +1,69 @@
 (function (ng) {
 
-    var mod = ng.module("post_colectivasModule", ['ui.router']);
+	var mod = ng.module("post_colectivasModule", ['ui.router']);
 
-    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+	mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
-    }]);
+	}]);
 
 })(window.angular);
 
 (function (ng) {
 
-    var mod = ng.module("post_colectivasModule");
-    mod.constant("post_colectivasContext", "api/post_colectivas");
+	var mod = ng.module("post_colectivasModule");
+	mod.constant("post_colectivasContext", "api/post_colectivas");
 
-    mod.controller('post_colectivasController', ['$scope', '$http', 'post_colectivasContext',
+	mod.controller('post_colectivasController', ['$scope', '$http', 'post_colectivasContext',
 
-        function ($scope, $http, post_colevtivasContext) {
+		function ($scope, $http, post_colevtivasContext) {
 
-            $scope.invoice = {
-                    items: [{
-                        id_cliente: 31,
-                        id_reserva: 811,
-                        cantidad_personas: 2
-                    }]
-                },
+		$scope.invoice = {
+				items: [{
+					id_cliente: 31,
+					id_reserva: 811,
+					cantidad_personas: 2
+				}]
+		},
 
-                $scope.addItem = function () {
-                    $scope.invoice.items.push({
-                        id_cliente: $scope.invoice.items[0].id_cliente + $scope.invoice.items.length,
-                        id_reserva: $scope.invoice.items[0].id_reserva + $scope.invoice.items.length,
-                        cantidad_personas: 2
-                    });
-                },
+		$scope.addItem = function () {
+			$scope.invoice.items.push({
+				id_cliente: $scope.invoice.items[0].id_cliente + $scope.invoice.items.length,
+				id_reserva: $scope.invoice.items[0].id_reserva + $scope.invoice.items.length,
+				cantidad_personas: 2
+			});
+		},
 
-                $scope.removeItem = function (index) {
-                    $scope.invoice.items.splice(index, 1);
-                },
+		$scope.removeItem = function (index) {
+			$scope.invoice.items.splice(index, 1);
+		},
 
-                $scope.enviar = function () {
+		$scope.enviar = function () {
 
-                    var us = angular.toJson($scope.invoice.items);
-                    console.log(us)
-                console.log($scope.servicios);
+			var us = angular.toJson($scope.invoice.items);
+			console.log(us)
+			console.log($scope.servicios);
 
-                    var data = {
-                        id: $scope.id,
-                        duracion: $scope.duracion,
-                        fecha_inicio_estadia: $scope.fecha,
-                        tipo_inmueble: $scope.tipo,
-                        privacidad: $scope.privacidad,
-                        cantidad_inmuebles: $scope.cantidad,
-                        servicios_deseados: $scope.servicios,
-                        usuarios: us
-                    };
+			var data = {
+					id: $scope.id,
+					duracion: $scope.duracion,
+					fecha_inicio_estadia: $scope.fecha,
+					tipo_inmueble: $scope.tipo,
+					privacidad: $scope.privacidad,
+					cantidad_inmuebles: $scope.cantidad,
+					servicios_deseados: $scope.servicios,
+					usuarios: us
+			};
 
-                    // DIRECCION HTTP 
-                    $http.post('http://localhost:8080/Alohandes_IT1/rest/reservasColectivas', data).then(function (response) {
+			// DIRECCION HTTP 
+			$http.post('http://localhost:8080/Alohandes_IT1/rest/reservasColectivas', data).then(function (response) {
+				$scope.post_data = response.data;
+				console.log(response.data);
+				console.log('Reserva Lista');
+			});
 
-                        $scope.post_data = response.data;
+		}
 
-                    }).then(function (response) {
-						alert(response);
-					});
+	}
 
-                }
-
-        }
-
-    ]);
+	]);
 })(window.angular);
